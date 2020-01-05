cbf8e091a27910bc7ca098082d4f8cf5ea9c7368
JoeyDeckers <deckersjoey@gmail.com>
2020-01-06 00:30:01 +0100

    Some changes

---
+++ ContainerVervoerUnitTests/ShipTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContainerVervoerUnitTests
+{
+    [TestClass]
+    public class ShipTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
